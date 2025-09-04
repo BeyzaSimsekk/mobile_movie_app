6ed1919d102b9801e77f6f382198b159ccb2e218
Beyza <u.beyza.simsek@gmail.com>
2025-09-05 00:05:56 +0300

    route & nav, done

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,31 +2,69 @@ import { icons } from '@/constants/icons'
 import { images } from '@/constants/images'
 import { Tabs } from 'expo-router'
 import React from 'react'
-import { Image, ImageBackground, Text } from 'react-native'
+import { Image, ImageBackground, Text, View } from 'react-native'
+
+const TabIcon = ({focused, icon, title} : any) => {
+
+    if(focused){
+    return (
+        <ImageBackground
+            source={images.highlight}
+            className='navbar-bg'
+        >
+            <Image 
+                source={icon}
+                tintColor= "#151312"
+                className='size-5'
+            />
+            <Text className='navbar-text'>{title}</Text>
+
+        </ImageBackground>
+    )
+    }
+
+    return (
+        <View className='size-full justify-center items-center mt-4 rounded-full'>
+            <Image source={icon} tintColor="#A8B5DB" className='size-5'/>
+        </View>
+    )
+}
 
 const _layout = () => {
   return (
-    <Tabs>
+    <Tabs
+        screenOptions={{
+            tabBarShowLabel: false,
+            tabBarItemStyle: {
+                width: "100%",
+                height: "100%",
+                justifyContent: 'center',
+                alignItems: 'center'
+            },
+            tabBarStyle: {
+                backgroundColor: '#0F0D23',
+                borderRadius: 50,
+                marginHorizontal: 20,
+                marginBottom: 36,
+                height: 52,
+                position: 'absolute',
+                overflow: 'hidden',
+                borderWidth: 1,
+                borderColor: '#0F0D23'
+            }
+        }}
+    >
         <Tabs.Screen 
             name='index'
             options={{
                 title: 'Home',
                 headerShown: false,
                 tabBarIcon: ({focused}) => (
-                    <>
-                        <ImageBackground
-                        source={images.highlight}
-                        className='navbar-bg'
-                        >
-                            <Image 
-                                source={icons.home}
-                                tintColor= "#151312"
-                                className='size-5'
-                            />
-                            <Text>Home</Text>
-
-                        </ImageBackground>
-                    </>
+                    <TabIcon 
+                        focused = {focused} 
+                        icon= {icons.home} 
+                        title='Home'
+                    />
                 )
             }}
         />
@@ -35,7 +73,14 @@ const _layout = () => {
             name='search'
             options={{
                 title: 'Search',
-                headerShown: false
+                headerShown: false,
+                tabBarIcon: ({focused}) => (
+                    <TabIcon 
+                        focused = {focused} 
+                        icon= {icons.search} 
+                        title='Search'
+                    />
+                )
             }}
         />
 
@@ -43,7 +88,14 @@ const _layout = () => {
             name='saved'
             options={{
                 title: 'Saved',
-                headerShown: false
+                headerShown: false,
+                tabBarIcon: ({focused}) => (
+                    <TabIcon 
+                        focused = {focused} 
+                        icon= {icons.save} 
+                        title='Saved'
+                    />
+                )
             }}
         />
 
@@ -51,7 +103,14 @@ const _layout = () => {
             name='profile'
             options={{
                 title: 'Profile',
-                headerShown: false
+                headerShown: false,
+                tabBarIcon: ({focused}) => (
+                    <TabIcon 
+                        focused = {focused} 
+                        icon= {icons.person} 
+                        title='Profile'
+                    />
+                )
             }}
         />
     </Tabs>
