5c65b166c5f7cd4812cb8f249a1335e3613e8193
Beyza <u.beyza.simsek@gmail.com>
2025-09-18 16:13:43 +0300

    auth sekmeleri başlangıç

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,12 +1,29 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+//import useAuthStore from '@/store/auth.store';
+import { icons } from '@/constants/icons';
+import { images } from '@/constants/images';
+import { Redirect, Slot } from 'expo-router';
+import React from 'react';
+import { Dimensions, Image, KeyboardAvoidingView, Platform, ScrollView, View } from 'react-native';
 
-const AuthLayout = () => {
+
+export default function AuthLayout() {
+
+  // const {isAuthenticated} = useAuthStore();
+   const isAuthenticated = false;
+
+   if(isAuthenticated) return <Redirect href="/" />
+
+
   return (
-    <View>
-      <Text>Auth Layout</Text>
-    </View>
+    <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+      <ScrollView className='bg-primary h-full' keyboardShouldPersistTaps="handled" >
+        <View className='w-full relative' style={{height: Dimensions.get('screen').height / 2.25}} >
+        <Image source={images.bg} className='absolute w-full z-0'/>
+        <Image source={icons.logo} className = "home-logo"/>
+        </View>
+        
+      <Slot />
+      </ScrollView>
+    </KeyboardAvoidingView>
   )
 }
-
-export default AuthLayout
