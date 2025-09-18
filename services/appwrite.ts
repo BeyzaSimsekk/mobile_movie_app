64b01e184d5d5bd191bf13666e75c1acf55efeb2
Beyza <u.beyza.simsek@gmail.com>
2025-09-18 16:14:45 +0300

    sign-in, sign up apis(?)

--- services/appwrite.ts
+++ services/appwrite.ts
@@ -1,20 +1,36 @@
-import { Client, Databases, ID, Query } from "react-native-appwrite";
+import { CreateUserParams, Movie, SignInParams, TrendingMovie } from "@/interfaces/interfaces";
+import { Account, Avatars, Client, Databases, ID, Query } from "react-native-appwrite";
 
 // track the searches made by a user
 
-const DATABASE_ID = process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID!;
-const METRICS_TABLE_ID = process.env.EXPO_PUBLIC_APPWRITE_METRICS_TABLE_ID!;
+const appwriteConfig ={
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!,
+    databaseId: process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID!,
+    platform: "com.byz.molixapp",
+    metricsTableId: process.env.EXPO_PUBLIC_APPWRITE_METRICS_TABLE_ID!,
+    userTableId:process.env.EXPO_PUBLIC_APPWRITE_USER_TABLE_ID!
+}
 
-const client = new Client()
-    .setEndpoint('https://cloud.appwrite.io/v1')
-    .setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!)
+
+export const client = new Client()
+
+client
+    .setEndpoint('https://cloud.appwrite.io/v1') 
+    .setProject(appwriteConfig.projectId)
+    .setPlatform(appwriteConfig.platform)
+
+export const account = new Account(client);
+export const databases = new Databases(client)
+//export const storage= new Storage(client);
+const avatars = new Avatars(client);
+
 
 const database = new Databases(client);
 
 export const updateSearchCount = async (query: string, movie: Movie) => {
 
     try {
-        const result = await database.listDocuments(DATABASE_ID, METRICS_TABLE_ID, [
+        const result = await database.listDocuments(appwriteConfig.databaseId, appwriteConfig.metricsTableId, [
             Query.equal("searchTerm", query),
     ]);
 
@@ -24,15 +40,15 @@ export const updateSearchCount = async (query: string, movie: Movie) => {
         const existingMovie = result.documents[0];
 
         await database.updateDocument(
-            DATABASE_ID,
-            METRICS_TABLE_ID,
+            appwriteConfig.databaseId,
+            appwriteConfig.metricsTableId,
             existingMovie.$id,
             {
                 count: existingMovie.count + 1
             }
         )
     } else {
-        await database.createDocument(DATABASE_ID, METRICS_TABLE_ID, ID.unique(), {
+        await database.createDocument(appwriteConfig.databaseId, appwriteConfig.metricsTableId, ID.unique(), {
             searchTerm: query,
             movie_id: movie.id,
             count: 1,
@@ -51,7 +67,7 @@ export const updateSearchCount = async (query: string, movie: Movie) => {
 
 export const getTrendingMovies = async (): Promise<TrendingMovie[] | undefined> => {
     try {
-        const result = await database.listDocuments(DATABASE_ID, METRICS_TABLE_ID, [
+        const result = await database.listDocuments(appwriteConfig.databaseId, appwriteConfig.metricsTableId, [
             Query.limit(5),
             Query.orderDesc("count")
     ]);
@@ -62,4 +78,73 @@ export const getTrendingMovies = async (): Promise<TrendingMovie[] | undefined> 
         console.log(error);
         return undefined;
     }
-}
+} //bunlara dikkat et !!!! interface değiştikten sonra hata vermişti!!!
+
+ export const createUser = async({email, password, name} : CreateUserParams) => {
+     try {
+        
+         const newAccount = await account.create(ID.unique(),email, password, name);
+
+         if(!newAccount) {
+             throw new Error("Failed to create user account");
+         }
+
+         // kullanıcı oluşturulduktan sonra otomatik giriş
+         await signIn({email, password})
+
+         const avatarUrl =  avatars.getInitialsURL(name)
+
+         const newUser = await databases.createDocument(
+             appwriteConfig.databaseId,
+             appwriteConfig.userTableId,
+             ID.unique(),
+             { email, name, accountId: newAccount.$id, avatar: avatarUrl }
+         );
+
+         return newUser;
+     } catch (error) {
+         throw new Error(error as string);
+     }
+ }
+
+
+ export const signIn = async({email, password} : SignInParams) => {
+     try {
+
+         // önce mevcut session var mı diye bak
+     const existing = await account.get().catch(() => null);
+
+     if (existing) {
+       // zaten login, yeni session açma
+       return existing;
+     }
+
+         // session yoksa giriş yap
+     return await account.createEmailPasswordSession(email, password);
+     } catch (error) {
+         throw new Error(error as string);
+     }
+ }
+
+// export const getCurrentUser = async () => {
+//     try {
+//         const currentAccount = await account.get();
+//         if(!currentAccount) {
+//             throw new Error("No user is currently signed in");
+//         }
+
+//         const currentUser = await databases.listDocuments(
+//             appwriteConfig.databaseId,
+//             appwriteConfig.userCollectionId,
+//             [Query.equal('accountId', currentAccount.$id)]
+//         )
+
+//         if(!currentAccount) throw Error;
+
+//         return currentUser.documents[0];
+
+//     } catch (error) {
+//         console.log(error);
+//         throw new Error(error as string);
+//     }
+// }
