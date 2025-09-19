42aa269cdce7363eb32f7cf4ff1dfa348605dcad
Beyza <u.beyza.simsek@gmail.com>
2025-09-19 22:12:21 +0300

    custom input buttom done but expo wont start...
    çalışan kodum açılmıyor bile artık...

--- interfaces/interfaces.d.ts
+++ interfaces/interfaces.d.ts
@@ -85,6 +85,24 @@ interface Props {
   onChangeText?: (text: string) => void;
 }
 
+interface CustomInputProps {
+    placeholder?: string;
+    value?: string;
+    onChangeText?: (text: string) => void;
+    label: string;
+    secureTextEntry?: boolean;
+    keyboardType?: "default" | "email-address" | "numeric" | "phone-pad";
+}
+
+interface CustomButtonProps {
+    onPress?: () => void;
+    title?: string;
+    style?: string;
+    leftIcon?: React.ReactNode;
+    textStyle?: string;
+    isLoading?: boolean;
+}
+
 interface MovieInfoProps{
   label: string;
   value?: string | number | null;
